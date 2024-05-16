1223cd431d3d8abea561ec3b3fea7dbc24990aaa
Sol Shenker <sol.shenker@gmail.com>
2024-05-16 10:36:29 -0400

    implement the CFD score and add some test data based on supplementary table 20

--- R/CFDscore.R
+++ R/CFDscore.R
@@ -3,3 +3,81 @@ package_state <- new.env()
 .onLoad <- function(libname, pkgname) {
     package_state$activity_scores <- readr::read_rds(fs::path_package("extdata","cfd_activity_scores.rds",package='CFDscoRe'))
 }
+
+#' CFD Score
+#'
+#' Calculates the CFD Score for a given guide/target site alignment.
+#'
+#' @param rna Character representation of the guide portion of a guide/target site alignment. Should be represented as DNA, valid characters include ['A','C','G','T','-']. Must be 20 nucleotides long.
+#' @param rna Character representation of the target portion of a guide/target site alignment. Should be represented as DNA, valid characters include ['A','C','G','T','-']. No length requirement.
+#' @param pam Character representation of the inferred PAM. A 'GG' PAM will receive no penalty.
+#' @export
+cfd_score <- function(rna,dna,pam){
+  position_table <- alignment_to_position_table(toupper(rna),toupper(dna))
+  classified_positions <- classify_positions(position_table)
+  score_positions(classified_positions,pam)
+}
+
+
+alignment_to_position_table <- function(rna,dna){
+  tibble(
+    rna = strsplit(rna,"")[[1]],
+    dna = strsplit(dna,"")[[1]]
+  ) %>%
+    mutate(index=row_number()) %>%
+    mutate(is_dna_bulge = rna == '-') %>%
+    mutate(tot_delete = cumsum(is_dna_bulge)) %>%
+    mutate(tot_offset = c(0,tot_delete) %>% head(n=n())) %>%
+    mutate(index=index-tot_offset) %>%
+    select(-is_dna_bulge,-tot_delete,-tot_offset)
+}
+
+classify_positions <- function(position_table) {
+  position_table %>%
+    mutate(matching = case_when(
+      rna == dna ~ 'match',
+      rna == '-' ~ 'delete',
+      dna == '-' ~ 'insert',
+      rna != dna ~ 'mismatch'
+    ))
+}
+
+score_positions <- function(classified_positions,PAM){
+  pam_score <- package_state$activity_scores$pam %>%
+    filter(pam==PAM) %>%
+    pull(activity)
+
+  mismatch_positions <- classified_positions %>%
+    filter(matching=='mismatch')
+  insert_positions <- classified_positions %>%
+    filter(matching=='insert')
+  delete_positions <- classified_positions %>%
+    filter(matching=='delete')
+
+  mismatch_scores <- score_mismatches(mismatch_positions)$activity
+  insert_scores <- score_inserts(insert_positions)$activity
+  delete_scores <- score_deletes(delete_positions)$activity
+
+  prod(c(1,pam_score,mismatch_scores,insert_scores,delete_scores))
+}
+
+dna_complement <- c('A'='T','C'='G','G'='C','T'='A')
+
+score_mismatches <- function(mismatched_positions){
+  mismatched_positions %>%
+    mutate(dna=dna_complement[dna]) %>%
+    left_join(package_state$activity_scores$mismatch,by=join_by(rna, dna, index))
+}
+
+score_inserts <- function(insert_positions){
+  insert_positions %>%
+    dplyr::rename(insertion=rna) %>%
+    left_join(package_state$activity_scores$rna_bulge,by=join_by(insertion, index))
+}
+
+score_deletes <- function(delete_positions){
+  delete_positions %>%
+    mutate(dna=dna_complement[dna]) %>%
+    dplyr::rename(deletion=dna) %>%
+    left_join(package_state$activity_scores$dna_bulge,by=join_by(deletion, index))
+}
--- tests/testthat/test-CFDscore.R
+++ tests/testthat/test-CFDscore.R
@@ -1,4 +1,16 @@
-test_that("verify loading of activity scores", {
+test_that("activity scores load correctly", {
     expect_equal( length( package_state$activity_scores ) , 4 )
     expect_true( length( setdiff( names(package_state$activity_scores), c('dna_bulge', 'rna_bulge', 'mismatch', 'pam') ) ) == 0 )
+})
+
+test_that("obtain same CFD scores as from Supp Table 20", {
+    expect_equal( cfd_score('AGGGGCCGGAGTATTGGGAG','AGGGGCCCGAGTATTGGGAG','GG'), 0.615384615 )
+    expect_equal( cfd_score('TCCACAGATACCTGAAGAAC','TCCGCAGATACCTGAAGAAC','GG'), 0.625 )
+    expect_equal( cfd_score('CCAGGGCGGCCGCCAGCAGC','CCAGGGCGGCCGCCAACAGC','GG'), 1 )
+    expect_equal( cfd_score('ATGTCTCTCCGAGATTGTAA','CTGTCTCTCCCAGATTGTAA','GG'), 0.367346939 )
+    expect_equal( cfd_score('CATGCCGTGTGTACCATGAG','CATGCCATGTGTACCATCAG','GG'), 0.476190476 )
+    expect_equal( cfd_score('CTACCTGGAGGGCGAGTGCG','CTACCTGGAGGGCACGTGCG','GG'), 0.204545455 )
+    expect_equal( cfd_score('CGACGCAAGTGGGAGCAGAG','AAACACAAGTGGGAGCAGGC','GG'), 0.117857143 )
+    expect_equal( cfd_score('GGCCATCATTGGAGCTGTGG','AATAGTCACTGGAGCTGTGG','GG'), 0.293022734 )
+    expect_equal( cfd_score('CATTACAAGGCCTACCTGGA','AGACTCAGGGCCTACCTGGA','GG'), 0.09859944 )
 })
