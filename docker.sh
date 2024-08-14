80ae3807e667936b9dc8bec791bfda16c75d693e
Sol Shenker <sol.shenker@gmail.com>
2024-08-14 09:26:15 -0400

    add build files

---
+++ docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#docker login -u shenkers
+
+is_dirty=false
+
+if [ -n "$(git ls-files --others --exclude-standard)" ]; then
+    echo "There are untracked files."
+    is_dirty=true
+fi
+
+if git diff-index --quiet HEAD --; then
+    echo "There are modified files"
+    is_dirty=true
+fi
+
+if [ "$is_dirty" = true ]; then
+    docker_tag=shenkers/cfdscore-dev
+else
+    docker_tag=shenkers/cfdscore
+    git_commit_sha=$(git rev-parse --short HEAD)
+fi
+
+docker build -t ${docker_tag} ${git_commit_sha:+-t shenkers/cfdscore:$git_commit_sha} .
+
+#docker push shenkers/cfdscore
